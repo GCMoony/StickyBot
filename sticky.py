2874b3c23a90dcf97f9cdfa0998f04d36b5cc4cc
GesusKryst <47188474+GesusKryst@users.noreply.github.com>
2021-12-24 11:39:24 -0700

    Created todo list

--- sticky.py
+++ sticky.py
@@ -3,8 +3,20 @@ import sqlite3
 
 # Create bot client
 
+
 ## Bot Commands ##
 
 # Remove iFunny links
 
-# 
+
+
+## MC Database commands ##
+
+# Get list of coordinates
+
+# Add coordinate to coordinate list
+
+# Remove coordinate from coordinate list
+
+
+# Run bot client

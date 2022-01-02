f07de2af3c6457132396379ea751e3d86752f204
GesusKryst <47188474+GesusKryst@users.noreply.github.com>
2022-01-02 10:50:47 -0700

    Now listens to both messages and commands

--- sticky.py
+++ sticky.py
@@ -19,7 +19,6 @@ token = open("token.txt", "r").read()
 sticky = commands.Bot(command_prefix="!")
 
 # Connect with bot
-
 @sticky.event
 async def on_ready():
     print("Ready to go!")
@@ -32,15 +31,18 @@ async def on_ready():
 async def xy(ctx):
     print("Working xy")
     conn, curs = connectDB()
-    print(conn, curs)
-    
+    curs.execute("SELECT * FROM mcLocations")
+    results = "Location Name\tX\tY\n"
+    for item in curs.fetchall():
+        results += str(item) +"\n"
+    await ctx.channel.send(results)
 
 # Add coordinate to coordinate list
 @sticky.command()
 async def add(ctx, *arg):
     print("Working add")
     if len(arg) != 2:
-        await ctx.send("Try adding a coordinate like this:\n\n" +
+        await ctx.send("Try adding a coordinate like this:\n" +
             "\t'!add locationName xCoord yCoord'")
     else:
         pass
@@ -51,7 +53,7 @@ async def rm(ctx, locationName = None):
     if(locationName and len(locationName) == 1):
         print("yuh")
     else:
-        await ctx.send("Try removing a location like this: \n\n" +
+        await ctx.send("Try removing a location like this: \n" +
             "\t'!rm locationName'")
 
 
@@ -59,8 +61,8 @@ async def rm(ctx, locationName = None):
 
 
 # Remove iFunny links
-@sticky.event
-async def on_message(msg):
+@sticky.listen('on_message')
+async def noFunnyLinks(msg):
     if "ifunny" in msg.content.lower() and not msg.content.startswith("!"):
         await msg.delete()
 
